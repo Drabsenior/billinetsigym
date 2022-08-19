eb928c53f2f2ebd384942e0d52072bfc428a3b3b
Drabsenior <drabsenior@gmail.com>
2022-08-19 15:04:32 +0300

    dynamic_start

--- src/pages/Pricedetail/Pricedetail.jsx
+++ src/pages/Pricedetail/Pricedetail.jsx
@@ -5,6 +5,7 @@ import Navbar from "../../components/Navbar/Navbar";
 import Content1 from "../../components/Content1/Content1";
 import Footer from "../../components/Footer/Footer";
 import Sliderimage from "../../components/Slider/Sliderimage";
+import Experttrainers from "../Expert/Experttrainers";
 const Pricedetail = () => {
   return (
     <div className="predictdetailcontainer">
@@ -12,6 +13,7 @@ const Pricedetail = () => {
       <Detailcomponent />
       <Content1 />
       <Sliderimage />
+      <Experttrainers />
       <Footer />
     </div>
   );
